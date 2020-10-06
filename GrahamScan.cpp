21b569087a7a1befae5bfb3ccb2bfe1bf906b953
Shadi <floydanthebeast@gmail.com>
2020-10-06 18:21:09 +0300

    Implement stack for GrahamScan

--- GrahamScan.cpp
+++ GrahamScan.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
-
 using namespace std;
 
-template <typename T>
 class Stack {
 private:
-    T* arr;
-    int index;
+    int* arr;
+    int index = 0; // Индекс, куда в следующий раз вставится элемент
+    int capacity;
+    int maxCapacity = 1000;
 public:
-    void push(T elem);
-    T pop();
-    T top();
-    T nextToTop();
-}
+    Stack(int capacity) {
+        if (capacity > 0) {
+            this->capacity = capacity;
+            this->arr = new int[capacity];
+        }
+        else
+            throw "Размер стека должен быть положительным числом";
+    };
+
+    void push(int elem) {
+        this->arr[++this->index] = elem;
+    };
+
+    int pop() {
+        return this->arr[index--];
+    };
+
+    int top() {
+        return this->arr[index - 1];
+    };
+
+    int nextToTop() {
+        return this->arr[index - 2];
+    };
+};
 
 int main() {
     return 0;
